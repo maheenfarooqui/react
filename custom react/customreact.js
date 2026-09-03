a12e1b6e9afff38aad04491a5c13b2473ab71626
maheen <maheenzuhra@gmail.com>
2026-09-03 11:28:34 +0500

    make good approach

--- custom react/customreact.js
+++ custom react/customreact.js
@@ -1,9 +1,22 @@
 function customRender(cutomElemnt , container){
- const   domElemnt = document.createElement(cutomElemnt.type);
- domElemnt.innerHTML = cutomElemnt.children;
- domElemnt.setAttribute('href' ,cutomElemnt.props.href);
- domElemnt.setAttribute('target' , cutomElemnt.props.target);
- container.appendChild(domElemnt);
+    // its basic approach not good cause code is reapeat
+//  const   domElemnt = document.createElement(cutomElemnt.type);
+//  domElemnt.innerHTML = cutomElemnt.children;
+//  domElemnt.setAttribute('href' ,cutomElemnt.props.href);
+//  domElemnt.setAttribute('target' , cutomElemnt.props.target);
+//  container.appendChild(domElemnt);
+
+// so use loop
+
+const domElemnt = document.createElement(cutomElemnt.type);
+domElemnt.innerHTML = cutomElemnt.children;
+ for(prop in cutomElemnt.props){
+    if(prop === 'children') continue;
+    domElemnt.setAttribute(prop , cutomElemnt.props[prop])
+ }
+
+container.appendChild(domElemnt);
+
 }
 
 const cutomElemnt = {
